ad88461849e95e5bd12c630163b16797d94b37d8
michael <mkigotho2@gmail.com>
2015-08-19 00:17:47 +0300

    more imports for the main activity java file

--- app/src/main/java/com/creatives/mike/introo/MainActivity.java
+++ app/src/main/java/com/creatives/mike/introo/MainActivity.java
@@ -2,6 +2,8 @@ package com.creatives.mike.introo;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
